1de464ebdd1a2d11dbd2a27e12485af170681c0f
oshin <oshindhaliwal94@gmail.com>
2018-08-17 17:00:55 +0530

    my second assignment

---
+++ assignment-2.py
@@ -0,0 +1,34 @@
+#Question 1
+a=("Hello World")
+print(a)
+
+#Question 2
+str1="acad"
+str2="view"
+str3=str1+str2
+print(str3)
+
+#Question 3
+a=int(input("Enter the first value"))
+b=int(input("Enter the second value"))
+c=int(input("Enter the third value"))
+print("First value is %d\nSecond value is %d\nThird value is %d" %(a,b,c))
+
+#Question 4
+a=("Let's get started")
+print(a)
+
+#Question 5
+s="Acadview"
+course="Python"
+fees=5000
+print("Platform is %s\nCourse is %s\nFees is %d" %(s,course,fees))
+
+
+#Question 6
+r=float(input("Enter radius of circle"))
+pi=3.14
+area=pi*(r**2)
+print(area)
+
+
